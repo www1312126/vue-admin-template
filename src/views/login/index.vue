a6ed896d5040bc9ff85407a4fa9a25dd10e1a311
diaojianfeng <466283062@qq.com>
2019-07-30 10:41:26 +0800

    1.登录页面

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,53 +1,60 @@
 <template>
   <div class="login-container">
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
+      <div style="background-color: #fff; padding:22px 30px 0 40px">
+        <div class="title-container">
+          <div class="cloud">
+            <img src="../../assets/images/login/cloud.png">
+          </div>
+          <h3 class="title">学校体育课运动信息平台</h3>
+        </div>
 
-      <div class="title-container">
-        <h3 class="title">Login Form</h3>
+        <el-form-item prop="username">
+          <!--<span class="svg-container">
+            <svg-icon icon-class="user" />
+          </span>-->
+          <el-input
+            ref="username"
+            v-model="loginForm.username"
+            placeholder="账号"
+            name="username"
+            type="text"
+            tabindex="1"
+            auto-complete="on"
+            clearable
+          />
+          <span class="icon-account">
+            <img src="../../assets/images/login/icon-account.png">
+          </span>
+        </el-form-item>
+
+        <el-form-item prop="password">
+          <!--<span class="svg-container">
+            <svg-icon icon-class="password" />
+          </span>-->
+          <el-input
+            :key="passwordType"
+            ref="password"
+            v-model="loginForm.password"
+            :type="passwordType"
+            placeholder="密码"
+            name="password"
+            tabindex="2"
+            auto-complete="on"
+            @keyup.enter.native="handleLogin"
+          />
+          <span class="icon-password">
+            <img src="../../assets/images/login/icon-password.png">
+          </span>
+        </el-form-item>
+
+        <el-button :loading="loading" style="width:100%;margin: 0 0 30px 0;background-color: #FE8B5C; color:#fff;" @click.native.prevent="handleLogin">登录</el-button>
+
+        <div class="tips">
+          <span style="margin-right:20px;">username: admin</span>
+          <span> password: any</span>
+        </div>
       </div>
-
-      <el-form-item prop="username">
-        <span class="svg-container">
-          <svg-icon icon-class="user" />
-        </span>
-        <el-input
-          ref="username"
-          v-model="loginForm.username"
-          placeholder="Username"
-          name="username"
-          type="text"
-          tabindex="1"
-          auto-complete="on"
-        />
-      </el-form-item>
-
-      <el-form-item prop="password">
-        <span class="svg-container">
-          <svg-icon icon-class="password" />
-        </span>
-        <el-input
-          :key="passwordType"
-          ref="password"
-          v-model="loginForm.password"
-          :type="passwordType"
-          placeholder="Password"
-          name="password"
-          tabindex="2"
-          auto-complete="on"
-          @keyup.enter.native="handleLogin"
-        />
-        <span class="show-pwd" @click="showPwd">
-          <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
-        </span>
-      </el-form-item>
-
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
-
-      <div class="tips">
-        <span style="margin-right:20px;">username: admin</span>
-        <span> password: any</span>
-      </div>
-
     </el-form>
   </div>
 </template>
@@ -59,14 +66,14 @@ export default {
   data() {
     const validateUsername = (rule, value, callback) => {
       if (value.length === '') {
-        callback(new Error('Please enter the correct user name'))
+        callback(new Error('请输入账号'))
       } else {
         callback()
       }
     }
     const validatePassword = (rule, value, callback) => {
       if (value.length < 6) {
-        callback(new Error('The password can not be less than 6 digits'))
+        callback(new Error('密码不能少于6位数'))
       } else {
         callback()
       }
@@ -143,39 +150,29 @@ $cursor: #fff;
 
 @supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
   .login-container .el-input input {
-    color: $cursor;
+    //color: $cursor;
   }
 }
 
 /* reset element-ui css */
 .login-container {
-  .el-input {
-    display: inline-block;
-    height: 47px;
-    width: 85%;
-
-    input {
-      background: transparent;
-      border: 0px;
-      -webkit-appearance: none;
-      border-radius: 0px;
-      padding: 12px 5px 12px 15px;
-      color: $light_gray;
-      height: 47px;
-      caret-color: $cursor;
-
-      &:-webkit-autofill {
-        box-shadow: 0 0 0px 1000px $bg inset !important;
-        -webkit-text-fill-color: $cursor !important;
-      }
+  .icon-account,.icon-password {
+    position: absolute;
+    top: 5px;
+    left: 8px;
+    width: 18px;
+    height: 18px;
+    img {
+      width: 18px;
+      height: 18px;
     }
   }
-
-  .el-form-item {
-    border: 1px solid rgba(255, 255, 255, 0.1);
-    background: rgba(0, 0, 0, 0.1);
-    border-radius: 5px;
-    color: #454545;
+  .el-input {
+    input {
+      padding: 0 0 0 35px;
+      &:-webkit-autofill {
+      }
+    }
   }
 }
 </style>
@@ -188,7 +185,9 @@ $light_gray:#eee;
 .login-container {
   min-height: 100%;
   width: 100%;
-  background-color: $bg;
+  //background-color: $bg;
+  background: url("../../assets/images/login/bg.png") no-repeat;
+  background-size: 100% 100%;
   overflow: hidden;
 
   .login-form {
@@ -212,20 +211,23 @@ $light_gray:#eee;
     }
   }
 
-  .svg-container {
-    padding: 6px 5px 6px 15px;
-    color: $dark_gray;
-    vertical-align: middle;
-    width: 30px;
-    display: inline-block;
-  }
-
   .title-container {
     position: relative;
-
+    padding: 50px 0 0 0;
+    .cloud  {
+      position: absolute;
+      width: 80px;
+      top: 0;
+      left: 45px;
+      img {
+        width: 80px;
+        height: auto;
+      }
+    }
     .title {
       font-size: 26px;
-      color: $light_gray;
+      //color: $light_gray;
+      color: #626262;
       margin: 0px auto 40px auto;
       text-align: center;
       font-weight: bold;
